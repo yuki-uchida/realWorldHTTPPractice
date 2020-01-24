7ee139f8f341e9da8f48f7c22c5ef72e19041c36
yuki-uchida <rantarn0326@gmail.com>
2020-01-24 22:51:22 +0900

    end curl9

---
+++ real_world_http/chapter3/curl9.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("テキスト")
+	resp, err := http.Post("http://localhost:18888", "text/plain", reader)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+}
